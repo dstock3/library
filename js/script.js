371b475b3e12087ff1008a4fd6f748e901fc1955
David Stockdale <dstock3@gmail.com>
2021-07-11 23:47:24 -0500

    Started adding code to displayBooks function.

--- js/script.js
+++ js/script.js
@@ -38,8 +38,12 @@ function elementBuilder (elType, className, parent) {
     return newElement;
 };
 
-function displayBooks(library) {
+function displayBooks(library, parent) {
     for (i = 0; i < library.length; i++) {
-
+        let titleElement = elementBuilder("h2", "book-title", parent);
+        let titleContent = document.createTextNode(`${library[i].title}`);
+        titleElement.appendChild(titleContent);
     };
 };
+
+const body = document.getElementsByTagName("body")[0];
