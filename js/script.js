659cf6bd8b0033fa88d6ce20b130236e22d3cf09
David Stockdale <dstock3@gmail.com>
2021-07-11 23:17:40 -0500

    Added library array with three books; added book contructor and add function.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const gatsby = new Book(`The Great Gatsby`, `F Scott Fitzgerald`, 255, `yes`);
+const madam = new Book(`Madame Bovary`, `Gustave Flaubert`, 368, `no`);
+const meta = new Book(`The Metamorphosis`, `Franz Kafka`, 250, `yes`);
+
+let myLibrary = [gatsby, capital, meta];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+      if (read === `no`) {
+        let read = 'not yet read'
+        return `${title} by ${author}, ${pages}, ${read}`
+      } else if (read === `yes`) {
+        let read = 'read';
+        return `${title} by ${author}, ${pages}, ${read}`
+      }
+    }
+};
+
+function addBookToLibrary(book, library) {
+    library.push(book);
+};
